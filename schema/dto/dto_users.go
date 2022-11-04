c6402227bdd37e11fa1caf40d4159a3b3cf6b610
Alejandro Labourdette <jal.labourdette@gmail.com>
2022-11-03 22:18:03 -0400

    use "email" instead of "e-mail" as json property

--- schema/dto/dto_users.go
+++ schema/dto/dto_users.go
@@ -6,12 +6,12 @@ type User struct {
 	Passphrase string `json:"passphrase" validate:"required"`
 	FirstName  string `json:"firstname" validate:"required"`
 	LastName   string `json:"lastname" validate:"required"`
-	Email      string `json:"e-mail" validate:"required,email"`
+	Email      string `json:"email" validate:"required,email"`
 }
 
 type UserResponse struct {
 	Username  string `json:"username" validate:"required"`
 	FirstName string `json:"firstname" validate:"required"`
 	LastName  string `json:"lastname" validate:"required"`
-	Email     string `json:"e-mail" validate:"required,email"`
+	Email     string `json:"email" validate:"required,email"`
 }
