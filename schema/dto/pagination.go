5b7d224dfb188a5c05ec9fd842df56667ef95e19
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-11-21 02:28:48 -0500

    added string Pagination to avoid "Cannot unmarshal string into Go value of type int" error.
    another possible solution is to redefine the Unmarshal function, for more information visit https://stackoverflow.com/questions/21151765/cannot-unmarshal-string-into-go-value-of-type-int64

--- schema/dto/pagination.go
+++ schema/dto/pagination.go
@@ -1,11 +1,11 @@
 package dto
 
 type Pagination struct {
-	Limit      int         `json:"limit,omitempty" query:"limit"`
-	Page       int         `json:"page,omitempty" query:"page"`
+	Limit      int         `json:"limit,string,omitempty" query:"limit"`
+	Page       int         `json:"page,string,omitempty" query:"page"`
 	Sort       string      `json:"sort,omitempty" query:"sort"`
 	TotalRows  int64       `json:"total_rows"`
-	TotalPages int         `json:"total_pages"`
+	TotalPages int         `json:"total_pages,string"`
 	Rows       interface{} `json:"rows"`
 }
 
