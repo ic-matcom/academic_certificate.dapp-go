68e5b0f5db3a21770d3e91978953db52c99921df
Alejandro Labourdette <jal.labourdette@gmail.com>
2022-11-24 17:02:03 -0500

    roles constants moved to models package

--- schema/models/role.go
+++ schema/models/role.go
@@ -5,3 +5,12 @@ type Role struct {
 	Name        string `json:"name" gorm:"uniqueIndex" validate:"required"`
 	Description string `json:"description" validate:"required"`
 }
+
+const (
+	Role_Invalid          = "Inválido"
+	Role_SystemAdmin      = "Administrador de Sistema"
+	Role_CertificateAdmin = "Administrador de Certificados"
+	Role_Secretary        = "Secretario"
+	Role_Dean             = "Decano"
+	Role_Rector           = "Rector"
+)
